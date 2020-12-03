89dafc7223d40d2e43475bf3636fd1e745437b97
mp4 <puleri.mp4@gmail.com>
2020-12-03 06:57:07 -0800

    First round of styling and Email functionality

---
+++ dist/assets/img/portfolio/messages/create.sh
@@ -0,0 +1,15 @@
+API="http://localhost:4741"
+URL_PATH="/messages"
+
+curl "${API}${URL_PATH}" \
+  --include \
+  --request POST \
+  --header "Content-Type: application/json" \
+  --header "Authorization: Bearer ${TOKEN}" \
+  --data '{
+    "message": {
+      "body": "'"${BODY}"'"
+    }
+  }'
+
+  echo
